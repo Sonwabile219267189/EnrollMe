767017f9196fc4c3f9eb415b9b769bb4be3071a4
Sonwabile <219267189@mycput.ac.za>
2021-10-22 11:36:46 +0100

    Added 2 php files.One to connect to database. The other send Student Data

---
+++ database_conn_inc.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * This file is used to connect to the database
+ */
+
+ 
+ const DB_SERVER = "localhost";
+ const DB_USERNAME = "root";
+ const DB_PASSWORD = "";
+ const DATABASE = "ess_db";
+
+/*connect to database*/
+
+$connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DATABASE);
+
+if ($connection->connect_error){
+    die("Connection failed: " . $connection->connect_error);
+}
+else {
+    echo "Connection Made. ";
+}
+
+//Testing the connection by retrieving data
+?>
